dabdd4121900361aecedc3ef57397278beac2aa0
joelpawar08 <100923058+joelpawar08@users.noreply.github.com>
2022-03-14 22:02:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//turn on fucntion 
+
+
+function bulbOn() {
+    let  bulbobj= document.querySelector(".bulboff")
+    bulbobj.setAttribute('src', "bulbon.jpg")
+}
+function bulbOff() {
+    let  bulbobj= document.querySelector(".bulboff")
+    bulbobj.setAttribute('src', "download.jpg")
+}
